fa258d40353f02a4bbd6315ac38f86fa2312c0f7
Aline Santana Cordeiro <alinesantanacordeiro@gmail.com>
2020-04-08 16:15:14 -0300

    new benchmarks

---
+++ vecsum.c
@@ -0,0 +1,20 @@
+#include "../../intrinsics/vima/vima.hpp"
+
+#define VECTOR_SIZE 2048
+
+__v32s main(__v32s argc, char const *argv[]) {
+    __v32s size = atoi(argv[1]);
+    if (size != 0 && (size & (size - 1)) == 0){
+        __v32s v_size = (1024 * 1024 * size) / sizeof(__v32s);
+        __v32s *vector_a = (__v32s *)malloc(sizeof(__v32s) * v_size);
+        __v32s *vector_b = (__v32s *)malloc(sizeof(__v32s) * v_size);
+        __v32s *vector_c = (__v32s *)malloc(sizeof(__v32s) * v_size);
+        for (__v32s i = 0; i < v_size; i += VECTOR_SIZE) {
+            _vim2K_iadds(&vector_a[i], &vector_b[i], &vector_c[i]);
+        }
+    } else {
+        printf("Error! Size is not power of two!\n");
+        exit(1);
+    }
+    return 0;
+}
