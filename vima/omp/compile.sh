7d2255fdf9c0b6d3fac19dc3742916a99d0340b0
Sairo Santos <sairoraoni@gmail.com>
2020-07-09 14:52:56 -0300

    static flag on compilation scripts

--- vima/omp/compile.sh
+++ vima/omp/compile.sh
@@ -4,7 +4,7 @@ SIM_HOME=$HOME"/OrCS"
 PIN_HOME=$SIM_HOME"/trace_generator/pin"
 SINUCA_TRACER_HOME=$SIM_HOME"/trace_generator/extras/pinplay/bin/intel64/sinuca_tracer.so"
 CODE_HOME=$HOME"/orcs_validation/vima/omp"
-COMP_FLAGS="-O2 -DNOINLINE -fopenmp"
+COMP_FLAGS="-O2 -DNOINLINE -static -fopenmp"
 SIZES=(1)
 # 2 4 8 16 32 64)
 SIZES_MATMUL=(1)
--- x86/avx512/compile.sh
+++ x86/avx512/compile.sh
@@ -4,7 +4,7 @@ SIM_HOME=$HOME"/OrCS"
 PIN_HOME=$SIM_HOME"/trace_generator/pin"
 SINUCA_TRACER_HOME=$SIM_HOME"/trace_generator/extras/pinplay/bin/intel64/sinuca_tracer.so"
 CODE_HOME=$HOME"/orcs_validation/x86/avx512"
-COMP_FLAGS="-O2 -mavx2 -march=native"
+COMP_FLAGS="-O2 -static -mavx2 -march=native"
 SIZES=(1 2 4 8 16 32 64)
 SIZES_MATMUL=(1 2 4 8)
 
