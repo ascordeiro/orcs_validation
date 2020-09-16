be8312ff5c3b8656d02b9f26ddbaa727da61569f
Sairo Santos <sairoraoni@gmail.com>
2020-09-16 18:25:34 -0300

    matmul omp correction

--- vima/omp/matmul_omp.c
+++ vima/omp/matmul_omp.c
@@ -14,26 +14,29 @@ __v32s main(__v32s argc, char const *argv[]) {
         __v32f *matrix_b = (__v32f *)malloc(sizeof(__v32f) * m_size * (VECTOR_SIZE * n_vectors));
         __v32f *matrix_c = (__v32f *)malloc(sizeof(__v32f) * m_size * (VECTOR_SIZE * n_vectors));
         __v32f *aux_vec = (__v32f *)malloc(sizeof(__v32f) * VECTOR_SIZE * n_vectors);
-        __v32f *partial_sum = (__v32f *)malloc(sizeof(__v32f) * VECTOR_SIZE * n_vectors);
+        __v32f *partial_sum;
 
         __v32s i, j, k;
 
-        #pragma omp parallel shared (matrix_a, matrix_b, matrix_c) private (i, j, k, partial_sum)
+        #pragma omp parallel shared (matrix_a, matrix_b, matrix_c) private (partial_sum)
         {
             if (VECTOR_SIZE == 64){
                 #pragma omp for schedule (static)
-                for (i = 0; i < m_size; ++i) {
-                    for (j = 0; j < m_size; ++j) {
-                        for (k = 0; k < n_vectors; ++k) {
+                for (__v32s i = 0; i < m_size; ++i) {
+                    partial_sum = (__v32f *)malloc(sizeof(__v32f) * VECTOR_SIZE * n_vectors);
+                    for (__v32s j = 0; j < m_size; ++j) {
+                        for (__v32s k = 0; k < n_vectors; ++k) {
                             _vim64_fmuls(&matrix_a[(i * VECTOR_SIZE * n_vectors) + (k * VECTOR_SIZE)], &matrix_b[(j * VECTOR_SIZE * n_vectors) + (k * VECTOR_SIZE)], &aux_vec[k * VECTOR_SIZE]);
-                            _vim64_fcums(&aux_vec[k * VECTOR_SIZE], &partial_sum[k]);
+                            //_vim64_fcums(&aux_vec[k * VECTOR_SIZE], &partial_sum[k]);
                         }
                         _vim64_fcums(partial_sum, &matrix_c[(i * m_size) + j]);
                     }
+                    free (partial_sum);
                 }
             } else if (VECTOR_SIZE == 2048){
                 #pragma omp for schedule (static)
                 for (i = 0; i < m_size; ++i) {
+                    partial_sum = (__v32f *)malloc(sizeof(__v32f) * VECTOR_SIZE * n_vectors);
                     for (j = 0; j < m_size; ++j) {
                         for (k = 0; k < n_vectors; ++k) {
                             _vim2K_fmuls(&matrix_a[(i * VECTOR_SIZE * n_vectors) + (k * VECTOR_SIZE)], &matrix_b[(j * VECTOR_SIZE * n_vectors) + (k * VECTOR_SIZE)], &aux_vec[k * VECTOR_SIZE]);
@@ -41,6 +44,7 @@ __v32s main(__v32s argc, char const *argv[]) {
                         }
                         _vim2K_fcums(partial_sum, &matrix_c[(i * m_size) + j]);
                     }
+                    free (partial_sum);
                 }
             } else printf ("VECTOR SIZE must be either 64 or 2048.\n");
         }
