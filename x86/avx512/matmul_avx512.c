d78a4e83991567c7f52a0b083620706d06aed777
Sairo Santos <sairoraoni@gmail.com>
2020-09-19 17:54:27 -0300

    tiled matmul x86

--- x86/avx512/matmul_avx512.c
+++ x86/avx512/matmul_avx512.c
@@ -3,23 +3,28 @@
 #include <math.h>
 #include <immintrin.h>
 
+int MIN (int a, int b){
+    if (a < b) return a;
+    else return b;
+}
+
 int main(int argc, char const *argv[]) {
     int size = atoi(argv[1]);
 
     if (size != 0 && (size & (size - 1)) == 0) {
         int m_size = sqrt((1024 * 1024 * size) / sizeof(float));
         while (m_size % 256 != 0) m_size++;
-        int n_vectors = ceil(((double) m_size/(double) 16));
-        float *matrix_a = (float *) aligned_alloc(32, sizeof (float) * m_size * (16 * n_vectors));
-        float *matrix_b = (float *) aligned_alloc(32, sizeof (float) * m_size * (16 * n_vectors));
-        float *matrix_c = (float *) aligned_alloc(32, sizeof (float) * m_size * (16 * n_vectors));
-        float sum, partial_sum;
+        float *matrix_a = (float *) aligned_alloc(32, sizeof (float) * m_size * m_size);
+        float *matrix_b = (float *) aligned_alloc(32, sizeof (float) * m_size * m_size);
+        float *matrix_c = (float *) aligned_alloc(32, sizeof (float) * m_size * m_size);
 
-        /*printf ("%d \n", m_size);
+        printf ("%d \n", m_size);
 
-        for (int x = 0; x < m_size*16*n_vectors; x++){
-            matrix_a[x] = rand() % 10 + 1;
-            matrix_b[x] = rand() % 10 + 1;
+        /*for (int x = 0; x < m_size; x++){
+            for (int y = 0; y < m_size; x++){
+                matrix_a[x*y] = rand() % 10 + 1;
+                matrix_b[x*y] = rand() % 10 + 1;
+            }
         }*/
 
         /*printf ("a: \n");
@@ -34,37 +39,45 @@ int main(int argc, char const *argv[]) {
         }
 
         printf ("\n");*/
+
+        int BLOCKSIZE = 64 / sizeof (float);
         
+        int i, j, k, i0, k0, j0;
         __m512 vec_a, vec_b, aux_vec;
-        for (int i = 0; i < m_size; ++i) {
-            for (int j = 0; j < m_size; ++j) {
-                sum = 0;
-                for (int k = 0; k < n_vectors; ++k) {
-                    vec_a = _mm512_load_ps (&matrix_a[(i * 16 * n_vectors) + (k * 16)]);
-                    //printf ("\nvec_a  : ");
-                    //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_a[i]);
-                    vec_b = _mm512_load_ps (&matrix_b[(j * 16 * n_vectors) + (k * 16)]);
-                    //printf ("\nvec_b  : ");
-                    //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_b[i]);
-                    aux_vec = _mm512_mul_ps (vec_a, vec_b);
-                    //printf ("\naux_vec  : ");
-                    //for (int i = 0; i < 16; i++) printf ("%.0f ", aux_vec[i]);
-                    
-                    for (int i = 0; i < 16; i++) sum += aux_vec[i];
-                    //printf ("a: %p | b: %p | aux: %p\n", &matrix_a[(i * 16 * n_vectors) + (k * 16)], &matrix_b[(j * 16 * n_vectors) + (k * 16)], &aux_vec[k * 16]);
+        for(i0 = 0; i0 < m_size; i0 += BLOCKSIZE) {
+            for(k0 = 0; k0 < m_size; k0 += BLOCKSIZE) {
+                for(j0 = 0; j0 < m_size; j0 += BLOCKSIZE) {
+                    for (i = i0; i < MIN(i0+BLOCKSIZE, m_size); i+=4) {
+                        for (j = j0; j < MIN(j0+BLOCKSIZE, m_size); j+=4)  {
+                            //printf ("C: i = %d, j = %d\n", i, j);
+                            aux_vec = _mm512_load_ps(&matrix_c[i*j]);
+                            for (k = k0; k < MIN(k0+BLOCKSIZE, m_size); k+=4) {
+                                //printf ("A: i = %d, k = %d\n", i, k);
+                                vec_a = _mm512_load_ps (&matrix_a[i*k]);
+                                //printf ("\nvec_a  : ");
+                                //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_a[i]);
+                                //printf ("B: k = %d, j = %d\n", k, j);
+                                vec_b = _mm512_load_ps (&matrix_b[k*j]);
+                                //printf ("\nvec_b  : ");
+                                //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_b[i]);
+                                aux_vec = _mm512_add_ps (aux_vec, _mm512_mul_ps (vec_a, vec_b));
+                            }
+                            //printf ("C: i = %d, j = %d\n", i, j);
+                            _mm512_store_ps(&matrix_c[i*j], aux_vec);
+                        }
+                    }
                 }
-                matrix_c[(i * m_size) + j] = sum;
-                //printf ("\nmatrix_c[%d] = %.0f", (i*m_size)+j, matrix_c[(i * m_size) + j]);
             }
         }
 
-        /*printf ("\n\nc: ");
-        for (int i = 0; i < m_size*16*n_vectors; i++) {
-            if (i % m_size == 0) printf ("\n");
-            printf ("%.0f ", matrix_c[i]);
-        }*/
+        printf ("\n\nc: ");
+        for (int i = 0; i < m_size; i++) {
+            for (int i = 0; i < m_size; i++) {
+                printf ("%.0f ", matrix_c[i*j]);
+            }
+        }
 
-        printf ("%f\n", matrix_c[m_size-1]);
+        //printf ("%f\n", matrix_c[m_size-1]);
 
         free(matrix_a);
         free(matrix_b);
--- x86/avx512_omp/matmul_avx512_omp.c
+++ x86/avx512_omp/matmul_avx512_omp.c
@@ -4,34 +4,52 @@
 #include <immintrin.h>
 #include <omp.h>
 
+
+int MIN (int a, int b){
+    if (a < b) return a;
+    else return b;
+}
+
 int main(int argc, char const *argv[]) {
     int size = atoi(argv[1]);
 
     if (size != 0 && (size & (size - 1)) == 0) {
         int m_size = sqrt((1024 * 1024 * size) / sizeof(float));
         while (m_size % 256 != 0) m_size++;
-        int n_vectors = ceil(((double) m_size/(double) 16));
-        float *matrix_a = (float *) aligned_alloc(32, sizeof (float) * m_size * (16 * n_vectors));
-        float *matrix_b = (float *) aligned_alloc(32, sizeof (float) * m_size * (16 * n_vectors));
-        float *matrix_c = (float *) aligned_alloc(32, sizeof (float) * m_size * (16 * n_vectors));
-        float sum;
+        float *matrix_a = (float *) aligned_alloc(32, sizeof (float) * m_size * m_size);
+        float *matrix_b = (float *) aligned_alloc(32, sizeof (float) * m_size * m_size);
+        float *matrix_c = (float *) aligned_alloc(32, sizeof (float) * m_size * m_size);
 
-        int i, j, k;
+        int BLOCKSIZE = 64 / sizeof (float);
+        
+        int i, j, k, i0, k0, j0;
         __m512 vec_a, vec_b, aux_vec;
-        #pragma omp parallel shared (vec_a, vec_b) private (i, j, k, sum, aux_vec)
+        #pragma omp parallel shared (vec_a, vec_b, matrix_a, matrix_b, matrix_c) private (i, j, k, sum, aux_vec)
         {
             #pragma omp for schedule (static)
-            for (i = 0; i < m_size; ++i) {
-                for (j = 0; j < m_size; ++j) {
-                    sum = 0;
-                    for (k = 0; k < n_vectors; ++k) {
-                        vec_a = _mm512_load_ps (&matrix_a[(i * 16 * n_vectors) + (k * 16)]);
-                        vec_b = _mm512_load_ps (&matrix_b[(j * 16 * n_vectors) + (k * 16)]);
-                        aux_vec = _mm512_mul_ps (vec_a, vec_b);
-                        
-                        for (int i = 0; i < 16; i++) sum += aux_vec[i];
+            for(i0 = 0; i0 < m_size; i0 += BLOCKSIZE) {
+                for(k0 = 0; k0 < m_size; k0 += BLOCKSIZE) {
+                    for(j0 = 0; j0 < m_size; j0 += BLOCKSIZE) {
+                        for (i = i0; i < MIN(i0+BLOCKSIZE, m_size); i+=4) {
+                            for (j = j0; j < MIN(j0+BLOCKSIZE, m_size); j+=4)  {
+                                //printf ("C: i = %d, j = %d\n", i, j);
+                                aux_vec = _mm512_load_ps(&matrix_c[i*j]);
+                                for (k = k0; k < MIN(k0+BLOCKSIZE, m_size); k+=4) {
+                                    //printf ("A: i = %d, k = %d\n", i, k);
+                                    vec_a = _mm512_load_ps (&matrix_a[i*k]);
+                                    //printf ("\nvec_a  : ");
+                                    //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_a[i]);
+                                    //printf ("B: k = %d, j = %d\n", k, j);
+                                    vec_b = _mm512_load_ps (&matrix_b[k*j]);
+                                    //printf ("\nvec_b  : ");
+                                    //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_b[i]);
+                                    aux_vec = _mm512_add_ps (aux_vec, _mm512_mul_ps (vec_a, vec_b));
+                                }
+                                //printf ("C: i = %d, j = %d\n", i, j);
+                                _mm512_store_ps(&matrix_c[i*j], aux_vec);
+                            }
+                        }
                     }
-                    matrix_c[(i * m_size) + j] = sum;
                 }
             }
         }
