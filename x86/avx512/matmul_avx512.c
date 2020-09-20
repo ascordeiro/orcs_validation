9621e0187b062ee130538a793f7ef260b18a2c93
Sairo Santos <sairoraoni@gmail.com>
2020-09-19 22:20:12 -0300

    new avx512 matmul

--- x86/avx512/matmul_avx512.c
+++ x86/avx512/matmul_avx512.c
@@ -3,11 +3,6 @@
 #include <math.h>
 #include <immintrin.h>
 
-int MIN (int a, int b){
-    if (a < b) return a;
-    else return b;
-}
-
 int main(int argc, char const *argv[]) {
     int size = atoi(argv[1]);
 
@@ -21,9 +16,9 @@ int main(int argc, char const *argv[]) {
         printf ("%d \n", m_size);
 
         /*for (int x = 0; x < m_size; x++){
-            for (int y = 0; y < m_size; x++){
-                matrix_a[x*y] = rand() % 10 + 1;
-                matrix_b[x*y] = rand() % 10 + 1;
+            for (int y = 0; y < m_size; y++){
+                matrix_a[x*m_size + y] = rand() % 10 + 1;
+                matrix_b[x*m_size + y] = rand() % 10 + 1;
             }
         }*/
 
@@ -37,47 +32,49 @@ int main(int argc, char const *argv[]) {
             if (i != 0 && i % m_size == 0) printf ("\n");
             printf ("%.0f ", matrix_b[i]);
         }
-
         printf ("\n");*/
 
-        int BLOCKSIZE = 64 / sizeof (float);
+        int BLOCKSIZE = 1024 / sizeof (float);
+        int count = 0;
         
         int i, j, k, i0, k0, j0;
         __m512 vec_a, vec_b, aux_vec;
         for(i0 = 0; i0 < m_size; i0 += BLOCKSIZE) {
             for(k0 = 0; k0 < m_size; k0 += BLOCKSIZE) {
                 for(j0 = 0; j0 < m_size; j0 += BLOCKSIZE) {
-                    for (i = i0; i < MIN(i0+BLOCKSIZE, m_size); i+=4) {
-                        for (j = j0; j < MIN(j0+BLOCKSIZE, m_size); j+=4)  {
+                    for (i = i0; i < i0+BLOCKSIZE; i++) {
+                        for (j = j0; j < j0+BLOCKSIZE; j+=16)  {
                             //printf ("C: i = %d, j = %d\n", i, j);
-                            aux_vec = _mm512_load_ps(&matrix_c[i*j]);
-                            for (k = k0; k < MIN(k0+BLOCKSIZE, m_size); k+=4) {
+                            aux_vec = _mm512_load_ps(&matrix_c[i*m_size + j]);
+                            for (k = k0; k < k0+BLOCKSIZE; k++) {
                                 //printf ("A: i = %d, k = %d\n", i, k);
-                                vec_a = _mm512_load_ps (&matrix_a[i*k]);
+                                vec_a = _mm512_load_ps (&matrix_a[i*m_size + k]);
                                 //printf ("\nvec_a  : ");
                                 //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_a[i]);
                                 //printf ("B: k = %d, j = %d\n", k, j);
-                                vec_b = _mm512_load_ps (&matrix_b[k*j]);
+                                vec_b = _mm512_load_ps (&matrix_b[k*m_size + j]);
                                 //printf ("\nvec_b  : ");
                                 //for (int i = 0; i < 16; i++) printf ("%.0f ", vec_b[i]);
                                 aux_vec = _mm512_add_ps (aux_vec, _mm512_mul_ps (vec_a, vec_b));
+                                count++;
                             }
                             //printf ("C: i = %d, j = %d\n", i, j);
-                            _mm512_store_ps(&matrix_c[i*j], aux_vec);
+                            _mm512_store_ps(&matrix_c[i*m_size + j], aux_vec);
                         }
                     }
                 }
             }
         }
 
-        printf ("\n\nc: ");
+        /*printf ("\n\nc: ");
         for (int i = 0; i < m_size; i++) {
-            for (int i = 0; i < m_size; i++) {
-                printf ("%.0f ", matrix_c[i*j]);
+            for (int j = 0; j < m_size; j++) {
+                printf ("%.0f ", matrix_c[i*m_size + j]);
             }
-        }
+        }*/
 
-        //printf ("%f\n", matrix_c[m_size-1]);
+        printf ("%f\n", matrix_c[m_size*m_size-1]);
+        printf ("count = %d\n", count);
 
         free(matrix_a);
         free(matrix_b);
