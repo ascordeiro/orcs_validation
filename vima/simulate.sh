f12317465878f6e760f4b6d9205877caeedb45bc
Sairo Santos <sairoraoni@gmail.com>
2021-10-13 15:24:13 -0300

    new simulate.sh

--- vima/simulate.sh
+++ vima/simulate.sh
@@ -4,7 +4,7 @@ SIM_HOME=$HOME"/OrCS"
 CODE_HOME=$HOME"/orcs_validation/vima"
 TRACE_HOME=$HOME"/orcs_validation/vima/traces"
 DATE_TIME=$(date '+%d%m%Y_%H%M%S');
-CACHE_SIZE=(64)
+CACHE_SIZE=(32 64 128 256 512 1024 2048 4096)
 
 cd $CODE_HOME
 
@@ -16,20 +16,22 @@ cd $TRACE_HOME
 for i in *_64B*.tid0.stat.out.gz
 do 
     cd $SIM_HOME
-    if [[ ${i%.c} != matmul.* && ${i%.c} != *_256* ]]; then
+    for j in "${CACHE_SIZE[@]}";
+    do
         TRACE=${i%.tid0.stat.out.gz}
-        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_256B_256_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &"
-        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_256B_256_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &
-    fi
+        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_1cores_${DATE_TIME}.txt &"
+        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_${DATE_TIME}.txt &
+    done
 done
 
 cd $TRACE_HOME
 for i in *_2048B*.tid0.stat.out.gz
 do 
     cd $SIM_HOME
-    if [[ ${i%.c} != matmul.* && ${i%.c} != *_256* ]]; then
+    for j in "${CACHE_SIZE[@]}";
+    do
         TRACE=${i%.tid0.stat.out.gz}
-        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_256_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &"
-        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_256_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_${DATE_TIME}.txt &
-    fi
+        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_1cores_${DATE_TIME}.txt &"
+        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_${DATE_TIME}.txt &
+    done
 done
