1697fefeecb85aae1d495a6d977ab0bdb2669e09
Sairo Santos <sairoraoni@gmail.com>
2021-10-14 15:34:23 -0300

    omp new options

--- vima/omp/compile.sh
+++ vima/omp/compile.sh
@@ -28,6 +28,7 @@ do
     rm exec/${i%.c}.out
     g++ $i $COMP_FLAGS -o exec/${i%.c}.out
     export OMP_NUM_THREADS=${THREADS}
+	if [[ ${i%.c} != matmul* ]]; then
     for j in "${SIZES[@]}";
 	do
 		for k in "${VECTOR[@]}";
@@ -36,5 +37,6 @@ do
 	    	nohup $PIN_HOME -t $SINUCA_TRACER_HOME -orcs_tracing 1 -trace iVIM -output $CODE_HOME/traces/${i%.c}.${k}B.${j}MB.${THREADS}t -threads ${THREADS} -- $CODE_HOME/exec/${i%.c}.out ${j} ${k} &> nohup.out
 		done
 	done
+	fi
 done
 done
