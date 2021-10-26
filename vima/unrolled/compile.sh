937162dda1bb4d7f94d77c9daf410a2570fd5914
Sairo Santos <sairoraoni@gmail.com>
2021-10-26 17:13:45 -0300

    unrolled versions

--- vima/unrolled/compile.sh
+++ vima/unrolled/compile.sh
@@ -3,10 +3,10 @@ HOME="/home/srsantos/Experiment"
 SIM_HOME=$HOME"/OrCS"
 PIN_HOME=$SIM_HOME"/trace_generator/pin"
 SINUCA_TRACER_HOME=$SIM_HOME"/trace_generator/extras/pinplay/bin/intel64/sinuca_tracer.so"
-CODE_HOME=$HOME"/orcs_validation/vima"
+CODE_HOME=$HOME"/orcs_validation/vima/unrolled"
 COMP_FLAGS="-O2 -DNOINLINE -static"
 VECTOR=(2048)
-SIZES=(32 64)
+SIZES=(1)
 SIZES_MATMUL=(1)
 
 cd $CODE_HOME
@@ -36,7 +36,7 @@ do
     	for j in "${SIZES_MATMUL[@]}";
     	do
     		echo "$PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}_${k}B.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} &> ${i%.c}.${j}MB_${k}B.1t.out &"
-    	    	nohup $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}_${k}B.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} 64 &> ${i%.c}.${j}MB_${k}B.1t.out &
+    	    	#nohup $PIN_HOME -t $SINUCA_TRACER_HOME -trace iVIM -output $CODE_HOME/traces/${i%.c}_${k}B.${j}MB.1t -- $CODE_HOME/exec/${i%.c}.out ${j} 64 &> ${i%.c}.${j}MB_${k}B.1t.out &
     	done
     fi
 done
---
+++ vima/unrolled/simulate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+HOME="/home/srsantos/Experiment"
+SIM_HOME=$HOME"/OrCS"
+CODE_HOME=$HOME"/orcs_validation/vima/unrolled"
+TRACE_HOME=$HOME"/orcs_validation/vima/unrolled/traces"
+DATE_TIME=$(date '+%d%m%Y_%H%M%S');
+CACHE_SIZE=(32)
+
+cd $CODE_HOME
+
+if [ ! -d "resultados" ]; then
+	mkdir -p "resultados"
+fi
+
+cd $TRACE_HOME
+for i in *_64B*.tid0.stat.out.gz
+do 
+    cd $SIM_HOME
+    for j in "${CACHE_SIZE[@]}";
+    do
+        TRACE=${i%.tid0.stat.out.gz}
+        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_1cores_${DATE_TIME}.txt &"
+        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_${DATE_TIME}.txt &
+    done
+done
+
+cd $TRACE_HOME
+for i in *_2048B*.tid0.stat.out.gz
+do 
+    cd $SIM_HOME
+    for j in "${CACHE_SIZE[@]}";
+    do
+        TRACE=${i%.tid0.stat.out.gz}
+        echo "./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_1cores_${DATE_TIME}.txt &"
+        nohup ./orcs -t ${TRACE_HOME}/${TRACE} -c configuration_files/vima_variations/sandy_vima_8K_${j}_1cores.cfg &> ${CODE_HOME}/resultados/${TRACE}_8K_${j}_${DATE_TIME}.txt &
+    done
+done
